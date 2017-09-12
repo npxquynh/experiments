9a371f1f75d0e5fe35513713e9b964d4273e7e3a
Quynh Nguyen <quynh.xq@gmail.com>
2017-09-12 18:30:37 +0200

    Initial commit

---
+++ test-schema-station-with-area/show/main.rb
@@ -0,0 +1,32 @@
+# /usr/bin/ruby
+require 'pry'
+require 'json'
+require 'jsonapi/parser'
+require 'json-schema'
+
+SCHEMA_PATH = './station_schema_knut.json'
+STATION_FILES = [
+  # './station_with_area.json',
+  './station_without_area.json'
+]
+
+WRONG_STATION_FILES = [
+  './wrong_station.json'
+]
+schema = JSON.parse(File.read(SCHEMA_PATH))
+
+begin
+  STATION_FILES.each do |station_file|
+    station = JSON.parse(File.read(station_file))
+    puts JSON::Validator.fully_validate(schema, station, strict: false)
+  end
+
+  puts "---------"
+  puts "ERRORS"
+  WRONG_STATION_FILES.each do |station_file|
+    station = JSON.parse(File.read(station_file))
+    puts JSON::Validator.fully_validate(schema, station)
+  end
+rescue JSON::Schema::ValidationError => e
+  e.message
+end
